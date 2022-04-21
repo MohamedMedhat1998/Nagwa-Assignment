78350853f2d8ccd17a03b997b8a5add7cc996026
Mohamed Medhat <mmedhat@instabug.com>
2022-04-21 16:27:32 +0200

    - Added "Hilt DI"

--- app/src/main/java/com/mohamed/medhat/nagwaassignment/MainActivity.kt
+++ app/src/main/java/com/mohamed/medhat/nagwaassignment/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.mohamed.medhat.nagwaassignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * Represents the home screen of the application. It contains the list of the available learning resources.
+ */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/mohamed/medhat/nagwaassignment/di/NagwaApp.kt
@@ -0,0 +1,11 @@
+package com.mohamed.medhat.nagwaassignment.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * An application class that is introduced here for the sake of di using hilt.
+ */
+@HiltAndroidApp
+class NagwaApp : Application() {
+}
